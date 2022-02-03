3c85e7d2d53863e40179836c1890bf1a5a2e9f76
pvvdg <vladpetrachuk@gmail.com>
2022-02-03 15:59:14 +0300

    feat: add solution 1.2

---
+++ 1_2.ps1
@@ -0,0 +1,17 @@
+# 2.	Используя скрипт (.ps1 файл) с параметрами. Сохранить в текстовый файл на диске список из N запущенных(!) служб. (зеленым цветом выделены обязательные входные параметры скрипта)
+
+Param (
+    [string]$file,
+    [string]$folder,
+    [int]$N
+)
+
+$Services = Get-Service | Where-Object { $_.Status -eq "Running" }
+
+if ($N -gt $Services.Count) {
+    Write-Host "All running services $($Services.Count). Enter number less then $N." -ForegroundColor Blue
+}
+else {
+    $Services[0..$($N - 1)] | Out-File "$($folder + '\' + $file + '.txt')"
+    Write-Host "Export seccess." -ForegroundColor Green
+}
